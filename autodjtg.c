001e837a340ae9c18ed67aef5fd4eb2d98011a7a
Tony Lee <github@roganartu.com>
2013-07-31 13:40:10 +1000

    Added main source files

---
+++ autodjtg.c
@@ -0,0 +1,21 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  autodjtg.c
+ *
+ *    Description:  Main autodjtg file
+ *
+ *        Version:  0.0.1
+ *        Created:  31/07/13 12:51:12
+ *
+ *         Author:  Tony Lee (Roganartu), github@roganartu.com
+ *
+ *          Notes:  
+ * =============================================================================
+ */
+#include <stdlib.h>
+#include "autodjtg.h"
+
+int main(int argc, char** argv) {
+    return 0;
+}
---
+++ autodjtg.h
@@ -0,0 +1,16 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  autodjtg.h
+ *
+ *    Description:  Main autodjtg header file
+ *
+ *        Version:  0.0.1
+ *        Created:  31/07/13 12:52:38
+ *
+ *         Author:  Tony Lee (Roganartu), github@roganartu.com
+ *
+ *          Notes:  
+ * =============================================================================
+ */
+
